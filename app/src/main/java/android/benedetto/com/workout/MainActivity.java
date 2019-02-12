65d18a318215c6b7bdde884d27751c0639aff112
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2019-02-12 07:58:40 +0100

    Add button and first fragment

--- app/src/main/java/android/benedetto/com/workout/MainActivity.java
+++ app/src/main/java/android/benedetto/com/workout/MainActivity.java
@@ -1,7 +1,9 @@
 package android.benedetto.com.workout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // if the button is pressed, we start the DetailActivity using an Intent
+    public void onShowDetails(View view) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
     }
 }
